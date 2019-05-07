c4faff80d29a6f3a2d8175c0d0d8016e3a8272e9
alexladines <alexladines@outlook.com>
2019-05-06 20:26:56 -0700

    Finish Part 1.1.

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Alex Ladines on 5/6/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    // MARK: - IBActions
+    @IBAction func searchTextFieldDidEndOnExit(_ sender: Any) {
+        
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Navigation
+    
+}
