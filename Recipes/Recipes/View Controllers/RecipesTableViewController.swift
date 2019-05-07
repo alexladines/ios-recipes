1171f3d415109e31b65bb5b53cc2cef98f5ee1e6
alexladines <alexladines@outlook.com>
2019-05-06 20:32:32 -0700

    Finish Part 1.2.

---
+++ Recipes/Recipes/View Controllers/RecipesTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecipesTableViewController.swift
+//  Recipes
+//
+//  Created by Alex Ladines on 5/6/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipesTableViewController: UITableViewController {
+    // MARK: - Properties
+    
+    // MARK: - IBOutlets
+    
+    // MARK: - IBActions
+    
+    // MARK: - Methods
+    
+    // MARK: - Life Cycle
+    
+    // MARK: - UITableViewDataSource
+    
+    // MARK: - UITableViewDelegate
+    
+    // MARK: - Navigation
+
+
+
+}
