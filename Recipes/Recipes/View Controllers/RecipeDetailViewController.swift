879a11e951e731b5ea629abf5e431bf3495c511b
alexladines <alexladines@outlook.com>
2019-05-06 20:43:35 -0700

    Finish Part 1.3 and auto layout.

---
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Alex Ladines on 5/6/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var recipeInstructionsTextView: UITextView!
+    
+    // MARK: - IBActions
+    
+    // MARK: - Methods
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Navigation
+    
+
+
+}
